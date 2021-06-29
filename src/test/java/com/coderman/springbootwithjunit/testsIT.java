6c7ec51687d73c9c31b258b1a99faab41a8da0f2
coderman <barslann58@gmail.com>
2021-06-29 00:18:35 -0700

    testing with different profiles

--- src/test/java/com/coderman/springbootwithjunit/testsIT.java
+++ src/test/java/com/coderman/springbootwithjunit/testsIT.java
@@ -17,7 +17,7 @@ public class testsIT {
 
     @Test
     public void getAuthorNameBooksTest() throws JSONException {
-        String expected = "[{\"id\":\"test123\",\"bookName\":\"spring\",\"isbn\":\"test\",\"author\":\"coderman\",\"aisle\":123},{\"id\":\"test124\",\"bookName\":\"react\",\"isbn\":\"test\",\"author\":\"coderman\",\"aisle\":124},{\"id\":\"test125\",\"bookName\":\"selenium\",\"isbn\":\"test\",\"author\":\"coderman\",\"aisle\":125}]";
+        String expected = "[{\"id\":\"test123\",\"bookName\":\"spring\",\"isbn\":\"test\",\"author\":\"coderman\",\"aisle\":123},{\"id\":\"test124\",\"bookName\":\"react\",\"isbn\":\"test\",\"author\":\"coderman\",\"aisle\":124},{\"id\":\"test125\",\"bookName\":\"selenium\",\"isbn\":\"test\",\"author\":\"coderman\",\"aisle\":125},{\"id\":\"abc123\",\"bookName\":\"Spring\",\"isbn\":\"abc\",\"author\":\"coderman\",\"aisle\":123}]\n";
         TestRestTemplate testRestTemplate = new TestRestTemplate();
         ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("http://localhost:9090/api/getBooks/author?authorname=coderman",String.class);
         System.out.println("Status code: " + responseEntity.getStatusCode());
@@ -26,17 +26,18 @@ public class testsIT {
     }
 
     @Test
-    public void addbookIntegrationTest() throws JSONException {
+    public void addBookIntegrationTest()  {
         TestRestTemplate testRestTemplate = new TestRestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<Book> request = new HttpEntity<Book>(buildABook(),headers);
-        ResponseEntity<String> responseEntity = testRestTemplate.postForEntity("http://localhost:9090/api/addbook/author?authorname=coderman",request,String.class);
+        ResponseEntity<String> responseEntity = testRestTemplate.postForEntity("http://localhost:9090/api/addbook",request,String.class);
         System.out.println("Status code: " + responseEntity.getStatusCode());
         System.out.println("Body: " + responseEntity.getBody());
         Assertions.assertEquals(HttpStatus.CREATED,responseEntity.getStatusCode());
         Assertions.assertEquals(buildABook().getId(),responseEntity.getHeaders().get("unique").get(0));
     }
+
 
     private Book buildABook(){
         Book book = new Book();
@@ -47,6 +48,7 @@ public class testsIT {
         book.setAuthor("coderman");
         return book;
     }
+
 
 
 }
